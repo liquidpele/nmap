0ad1833703a24e5c0e65863f338cfc8b738f9e9e
liquidpele <reecepegues+github@gmail.com>
2024-03-15 10:55:30 -0400

    Increase service probe line size, better error handling
    Increase the service probe line size possible to allow for adding larger probes, add a better error in the case that a line is too long.

--- service_scan.cc
+++ service_scan.cc
@@ -1303,7 +1303,8 @@ void ServiceProbe::addMatch(const char *match, int lineno) {
    (servicematch) which use this */
 void parse_nmap_service_probe_file(AllProbes *AP, const char *filename) {
   ServiceProbe *newProbe = NULL;
-  char line[2048];
+  /
+  char line[16384];
   int lineno = 0;
   FILE *fp;
 
@@ -1331,6 +1332,10 @@ void parse_nmap_service_probe_file(AllProbes *AP, const char *filename) {
     if (strncmp(line, "Probe ", 6) != 0)
       fatal("Parse error on line %d of nmap-service-probes file: %s -- line was expected to begin with \"Probe \" or \"Exclude \"", lineno, filename);
 
+    if (strnlen(line, sizeof(line)) >= sizeof(line) - 1) {
+      fatal("Parse error on line %d of nmap-service-probes file: %s -- line is too long for buffer, max length is %lu", lineno, filename, sizeof(line));
+    }
+
     newProbe = new ServiceProbe();
     newProbe->setProbeDetails(line + 6, lineno);
 
@@ -1339,6 +1344,8 @@ void parse_nmap_service_probe_file(AllProbes *AP, const char *filename) {
       lineno++;
       if (*line == '\n' || *line == '#')
         continue;
+
+      if strnlen(
 
       if (strncmp(line, "Probe ", 6) == 0) {
         if (newProbe->isNullProbe()) {
